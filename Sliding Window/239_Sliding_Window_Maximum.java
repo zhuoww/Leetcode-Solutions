ac75ee5761d6182fab67ca9355843de943cfb7db
zhuoww <zhuoww1008@gmail.com>
2023-06-19 20:21:25 -0700

    v5

--- Sliding Window/239_Sliding_Window_Maximum.java
+++ Sliding Window/239_Sliding_Window_Maximum.java
@@ -1,6 +1,7 @@
 import java.util.Deque;
 import java.util.LinkedList;
 
+//The MonotonicQueue class is used to maintain a monotonic decreasing order of elements
 class MonotonicQueue {
     Deque<Integer> deque = new LinkedList<>();
 
@@ -31,11 +32,13 @@ class Solution {
         int[] res = new int[nums.length - k + 1];
         int index = 0;
 
+        // adds the first k elements of nums to que
         for (int i = 0; i < k; i++) {
             que.add(nums[i]);
         }
         res[index++] = que.peek();
 
+        // iterates from k to the end of nums
         for (int i = k; i < nums.length; i++) {
             que.poll(nums[i - k]);
             que.add(nums[i]);

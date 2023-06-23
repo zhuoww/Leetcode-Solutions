b2aec861e702e45620b0a1f7ecf36b6743bf11f3
zhuoww <zhuoww1008@gmail.com>
2023-06-22 20:06:04 -0700

    v1

---
+++ Binary Search/138_Copy_List_with_Random_Pointer.java
@@ -0,0 +1,41 @@
+
+// Definition for a Node.
+
+import java.util.HashMap;
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        // time: O(n) -- n is the number of nodes in the linked list
+        // space: O(n) -- space is used to store the newly created copied nodes
+        if (head == null)
+            return null;
+        Node cur = head;
+        HashMap<Node, Node> map = new HashMap<>();
+
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+
+        return map.get(head);
+    }
+}

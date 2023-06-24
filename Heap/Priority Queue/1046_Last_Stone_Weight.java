551b660852ce83f3aff966316024cca572ab0308
zhuoww <zhuoww1008@gmail.com>
2023-06-24 15:08:49 -0700

    v1

---
+++ Heap/Priority Queue/1046_Last_Stone_Weight.java
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        // maxHeap
+        // time: O(nlogn)--each insertion operation (maxHeap.offer(stone)) takes O(logn)
+        // time, and there are n stones
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+
+        while (maxHeap.size() > 1) {
+            int first = maxHeap.poll();
+            int second = maxHeap.poll();
+
+            int newStone = first - second;
+
+            if (newStone > 0) {
+                maxHeap.offer(newStone);
+            }
+        }
+        return maxHeap.size() != 0 ? maxHeap.peek() : 0;
+    }
+}

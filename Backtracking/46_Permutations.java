44847c939c28c3d48b046b6b115428bed9491f50
zhuoww <zhuoww1008@gmail.com>
2023-06-25 13:54:29 -0700

    v1

---
+++ Backtracking/46_Permutations.java
@@ -0,0 +1,36 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution {
+    LinkedList<Integer> path = new LinkedList<>();
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums.length == 0)
+            return res;
+        boolean[] used = new boolean[nums.length];
+        backtracking(nums, used);
+        return res;
+    }
+
+    public void backtracking(int[] nums, boolean[] used) {
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] == true)
+                continue;
+            path.add(nums[i]);
+            used[i] = true;
+            backtracking(nums, used);
+            path.removeLast();
+            used[i] = false;
+        }
+    }
+
+}

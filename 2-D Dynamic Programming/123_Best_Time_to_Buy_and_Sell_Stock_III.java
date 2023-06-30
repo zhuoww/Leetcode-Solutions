9b4b838fc0d60089385cbade6948a383fc65f2a8
zhuoww <zhuoww1008@gmail.com>
2023-06-29 19:58:02 -0700

    v1

--- 2-D Dynamic Programming/123_Best_Time_to_Buy_and_Sell_Stock_III.java
+++ 2-D Dynamic Programming/123_Best_Time_to_Buy_and_Sell_Stock_III.java
@@ -5,19 +5,19 @@ class Solution {
         // dp
         if (prices == null || prices.length == 0)
             return 0;
-        int[][] dp = new int[prices.length][4];
-        dp[0][0] = -prices[0];
-        dp[0][1] = 0;
-        dp[0][2] = -prices[0];
-        dp[0][3] = 0;
+        int[][] dp = new int[prices.length][5];
+        dp[0][1] = -prices[0];
+        dp[0][2] = 0;
+        dp[0][3] = -prices[0];
+        dp[0][4] = 0;
 
         for (int i = 1; i < prices.length; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);// buying stock for the first time
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);// selling stock for the first time
-            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] - prices[i]);// buying stock for the second time
-            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] + prices[i]);// selling stock for the second time
+            dp[i][1] = Math.max(dp[i - 1][1], 0 - prices[i]);// buying stock for the first time
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);// selling stock for the first time
+            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);// buying stock for the second time
+            dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);// selling stock for the second time
         }
-        return dp[prices.length - 1][3];
+        return dp[prices.length - 1][4];
 
     }
 }

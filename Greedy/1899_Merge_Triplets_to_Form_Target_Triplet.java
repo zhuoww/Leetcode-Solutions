f7f19374cd2b06ac6fb51742915a2c3ba30f9c07
zhuoww <zhuoww1008@gmail.com>
2023-07-02 15:55:03 -0700

    v1

---
+++ Greedy/1899_Merge_Triplets_to_Form_Target_Triplet.java
@@ -0,0 +1,18 @@
+package Greedy;
+
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        boolean[] res = new boolean[3];
+
+        for (int i = 0; i < triplets.length; i++) {
+            if (triplets[i][0] > target[0] || triplets[i][1] > target[1] || triplets[i][2] > target[2])
+                continue;
+            for (int j = 0; j < 3; j++) {
+                if (triplets[i][j] == target[j]) {
+                    res[j] = true;
+                }
+            }
+        }
+        return res[0] && res[1] && res[2];
+    }
+}

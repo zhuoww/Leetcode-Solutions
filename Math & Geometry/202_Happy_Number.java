9df6d4ae25720e405c73aa39ebdeabe528b506e8
zhuoww <zhuoww1008@gmail.com>
2023-07-03 17:35:07 -0700

    v1

---
+++ Math & Geometry/202_Happy_Number.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+
+        while (n != 1) {
+            int sum = 0;
+            while (n > 0) {
+                sum += (n % 10) * (n % 10);
+                n = n / 10;
+            }
+            if (set.contains(sum)) {
+                return false;
+            }
+            set.add(sum);
+            n = sum;
+        }
+        return true;
+    }
+}

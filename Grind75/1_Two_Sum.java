d49c7ec6c62365b837a0435297fef1ef690c1ce3
zhuoww <zhuoww1008@gmail.com>
2023-07-05 12:46:50 -0700

    v1

---
+++ Grind75/1_Two_Sum.java
@@ -0,0 +1,26 @@
+package Grind75;
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // Tips: HashMap, temp = target - nums[i];
+        // time: O(n)
+        // space: O(n)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        if (nums == null || nums.length == 0)
+            return res;
+
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                res[1] = i;
+                res[0] = map.get(temp);
+                break;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+}

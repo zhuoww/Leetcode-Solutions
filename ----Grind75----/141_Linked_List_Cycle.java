042ba69cbfbf180138ff825514c87d8785b0a1a0
zhuoww <zhuoww1008@gmail.com>
2023-07-07 10:23:48 -0700

    v1

---
+++ ----Grind75----/141_Linked_List_Cycle.java
@@ -0,0 +1,26 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        next = null;
+    }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow)
+                return true;
+        }
+        return false;
+
+    }
+}

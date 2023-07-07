11db2fa95fbd731a2806440fa14f452f1feb1966
zhuoww <zhuoww1008@gmail.com>
2023-07-07 14:24:33 -0700

    v1

--- ----Grind75----/2024_Maximize_the_Confusion_of_an_Exam.java
+++ ----Grind75----/2024_Maximize_the_Confusion_of_an_Exam.java
@@ -1,3 +1,4 @@
+//mehtod 1
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
         // time: O(n)
@@ -32,6 +33,38 @@ class Solution {
             }
             r++;
             res = Math.max(res, r - l);
+        }
+        return res;
+    }
+}
+
+// method 2
+class Solution {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int[] freq = new int[2];
+        int res = 0;
+        int maxFreq = 0;
+        int l = 0;
+
+        for (int r = 0; r < answerKey.length(); r++) {
+            if (answerKey.charAt(r) == 'T') {
+                freq[0]++;
+            } else {
+                freq[1]++;
+            }
+
+            maxFreq = Math.max(freq[0], freq[1]);
+
+            if (r - l + 1 - maxFreq > k) {
+                if (answerKey.charAt(l) == 'T') {
+                    freq[0]--;
+                } else {
+                    freq[1]--;
+                }
+                l++;
+            }
+
+            res = Math.max(res, r - l + 1);
         }
         return res;
     }

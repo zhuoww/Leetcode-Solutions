5d3a1c0bdf149c597b8b6722c90190db29079484
zhuoww <zhuoww1008@gmail.com>
2023-07-12 22:23:53 -0700

    v1

---
+++ ----Grind75----/981_Time_Based_Key-Value_Store.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class TimeMap {
+    HashMap<String, List<Pair<String, Integer>>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        if (!map.containsKey(key)) {
+            map.put(key, new ArrayList<>());
+        }
+        map.get(key).add(new Pair(value, timestamp));
+    }
+
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key))
+            return "";
+        String res = "";
+        List<Pair<String, Integer>> list = map.get(key);
+        int l = 0;
+        int r = list.size() - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (list.get(mid).getValue() <= timestamp) {
+                res = list.get(mid).getKey();
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return res;
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */

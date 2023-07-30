d91055eb4870220d5ee49cb0b5b442e924b89b4a
zhuoww <zhuoww1008@gmail.com>
2023-07-30 16:04:09 -0700

    v1

---
+++ ----Grind75----/23_Merge_k_Sorted_Lists.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0)
+            return null;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.add(node);
+            }
+        }
+
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            cur.next = node;
+            cur = cur.next;
+            if (node.next != null) {
+                pq.add(node.next);
+            }
+        }
+        return dummy.next;
+
+    }
+}

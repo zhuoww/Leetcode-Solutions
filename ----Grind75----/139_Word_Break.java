f42b1139c3a7d1e45216235be0e9d6bafad06d8b
zhuoww <zhuoww1008@gmail.com>
2023-08-01 13:18:18 -0700

    v1

---
+++ ----Grind75----/139_Word_Break.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (set.contains(s.substring(j, i)) && dp[j] == true) {
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}

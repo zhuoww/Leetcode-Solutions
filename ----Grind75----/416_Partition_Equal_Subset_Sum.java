db6f99a4a084cd2763529bf80ed79f5b698a5d34
zhuoww <zhuoww1008@gmail.com>
2023-08-02 21:34:29 -0700

    v1

---
+++ ----Grind75----/416_Partition_Equal_Subset_Sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return true;
+        int len = nums.length;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0)
+            return false;
+        int target = sum / 2;
+
+        boolean[][] dp = new boolean[len + 1][target + 1];
+        for (int i = 0; i <= len; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i <= len; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (j < nums[i - 1]) {// 背包容量不足，不能装入第 i 个物品
+                    dp[i][j] = dp[i - 1][j];
+                } else {// 装入或不装入背包
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[len][target];
+
+    }
+}

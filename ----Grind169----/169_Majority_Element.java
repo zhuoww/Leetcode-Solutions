a9ab04dd4a44c24d2baef91644e948a4082ed2a9
zhuoww <zhuoww1008@gmail.com>
2023-08-04 14:29:02 -0700

    v1

---
+++ ----Grind169----/169_Majority_Element.java
@@ -0,0 +1,28 @@
+//sorting: time: O(nlogn), space: O(1)
+class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        Arrays.sort(nums);
+
+        return nums[nums.length / 2];
+    }
+}
+
+// HashMap: run time: 17 ms, time and space complexity: O(n)
+// key少，使用map快
+class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if (map.get(nums[i]) > nums.length / 2) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}

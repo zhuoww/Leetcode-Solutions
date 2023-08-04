c681dbd154aee5c4e7250b576ce48ca20785d519
zhuoww <zhuoww1008@gmail.com>
2023-08-04 10:22:15 -0700

    v1

--- ----Grind169----/238_Product_of_Array_Except_Self.java
+++ ----Grind169----/238_Product_of_Array_Except_Self.java
@@ -83,3 +83,22 @@ class Solution {
         return nums;
     }
 }
+
+// brute force
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int product = 1;
+            for (int j = 0; j < nums.length; j++) {
+                if (j != i) {
+                    product *= nums[j];
+                }
+                res[i] = product;
+            }
+        }
+        return res;
+
+    }
+}

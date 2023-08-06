c3d9510a3f569f9000343eba6a1546e0f71bbd4e
zhuoww <zhuoww1008@gmail.com>
2023-08-05 21:55:57 -0700

    v1

---
+++ ----Grind169----/134_Gas_Station.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        // greedy
+        int curSum = 0;
+        int totalSum = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            curSum += gas[i] - cost[i];
+            totalSum += gas[i] - cost[i];
+            if (curSum < 0) {
+                start = i + 1;
+                curSum = 0;
+            }
+        }
+        if (totalSum < 0)
+            return -1;
+        return start;
+    }
+}

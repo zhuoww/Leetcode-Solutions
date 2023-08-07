0cc94e92fe1857de00ca7a63da36e62001b4df8e
zhuoww <zhuoww1008@gmail.com>
2023-08-07 16:06:36 -0700

    v1

---
+++ ----Grind169----/525_Contiguous_Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i] == 0 ? -1 : 1;
+            if (map.containsKey(sum)) {
+                maxLength = Math.max(maxLength, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+        return maxLength;
+    }
+}

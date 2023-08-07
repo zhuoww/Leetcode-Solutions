f6d87d2fa8ce27c0652ef9141dfdae8dc6589028
zhuoww <zhuoww1008@gmail.com>
2023-08-07 16:16:50 -0700

    v1

--- ----Grind169----/525_Contiguous_Array.java
+++ ----Grind169----/525_Contiguous_Array.java
@@ -1,3 +1,4 @@
+//Hashmap, cumulative sum
 class Solution {
     public int findMaxLength(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -14,5 +15,26 @@ class Solution {
             }
         }
         return maxLength;
+    }
+}
+
+// brute-force
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int zeros = 0, ones = 0;
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] == 0) {
+                    zeros++;
+                } else {
+                    ones++;
+                }
+                if (zeros == ones) {
+                    count = Math.max(count, j - i + 1);
+                }
+            }
+        }
+        return count;
     }
 }

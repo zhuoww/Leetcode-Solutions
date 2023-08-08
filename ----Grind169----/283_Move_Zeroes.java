2495887a2fbe7bdbbf54236b52360398d53dc878
zhuoww <zhuoww1008@gmail.com>
2023-08-08 11:50:57 -0700

    v1

--- ----Grind169----/283_Move_Zeroes.java
+++ ----Grind169----/283_Move_Zeroes.java
@@ -1,3 +1,4 @@
+//1
 class Solution {
     public void moveZeroes(int[] nums) {
         if (nums == null || nums.length < 2)
@@ -12,6 +13,32 @@ class Solution {
 
         for (int i = slow; i < nums.length; i++) {
             nums[i] = 0;
+        }
+    }
+}
+
+// 2
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length < 2)
+            return;
+
+        int slow = 0;
+        int fast = 0;
+
+        while (fast < nums.length) {
+            if (nums[slow] != 0) {
+                slow++;
+                fast++;
+            } else if (nums[fast] != 0) {
+                int temp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = temp;
+                slow++;
+                fast++;
+            } else {
+                fast++;
+            }
         }
     }
 }

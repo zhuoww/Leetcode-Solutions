457c8b3fae7721a1230ee92d2860413923815183
zhuoww <zhuoww1008@gmail.com>
2023-08-07 17:50:54 -0700

    v1

--- ----Grind169----/560_Subarray_Sum_Equals_K.java
+++ ----Grind169----/560_Subarray_Sum_Equals_K.java
@@ -1,3 +1,4 @@
+//brute-force
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int len = nums.length;
@@ -9,6 +10,26 @@ class Solution {
                 if (sum == k)
                     res++;
             }
+        }
+        return res;
+    }
+}
+
+// prefix sum, hashmap
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int res = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                res += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+
         }
         return res;
     }

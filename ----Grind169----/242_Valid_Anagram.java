20d9f5329ffe94b0893cb46dd9ab7c5b9a2404c8
zhuoww <zhuoww1008@gmail.com>
2023-08-12 12:25:06 -0700

    v1

---
+++ ----Grind169----/242_Valid_Anagram.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        int[] res = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            res[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            res[t.charAt(i) - 'a']--;
+        }
+        for (int c : res) {
+            if (c != 0)
+                return false;
+        }
+        return true;
+    }
+}

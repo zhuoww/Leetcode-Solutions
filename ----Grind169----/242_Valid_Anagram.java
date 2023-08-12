84a212509438fa49f791404f42bbf55b457e0ebd
zhuoww <zhuoww1008@gmail.com>
2023-08-12 12:45:06 -0700

    v1

--- ----Grind169----/242_Valid_Anagram.java
+++ ----Grind169----/242_Valid_Anagram.java
@@ -38,3 +38,28 @@ class Solution {
 
     }
 }
+
+// follow-up question
+// HashMap
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (Character c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (Character c : t.toCharArray()) {
+            if (!map.containsKey(c))
+                return false;
+            map.put(c, map.get(c) - 1);
+
+            if (map.get(c) == 0) {
+                map.remove(c);
+            }
+
+        }
+        return map.isEmpty();
+    }
+}

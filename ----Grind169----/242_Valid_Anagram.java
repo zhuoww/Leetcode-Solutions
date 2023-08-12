813044384f3e9dbb37902b1c55a2180649f3d11a
zhuoww <zhuoww1008@gmail.com>
2023-08-12 12:25:29 -0700

    v1

--- ----Grind169----/242_Valid_Anagram.java
+++ ----Grind169----/242_Valid_Anagram.java
@@ -17,3 +17,24 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        int[] count = new int[26];
+        for (Character c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        for (Character c : t.toCharArray()) {
+            count[c - 'a']--;
+        }
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}

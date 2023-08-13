114b714cd0c79dc2af3a339f2871bac8511e7a69
zhuoww <zhuoww1008@gmail.com>
2023-08-13 15:40:07 -0700

    v1

--- ----Grind169----/438_Find_All_Anagrams_in_a_String.java
+++ ----Grind169----/438_Find_All_Anagrams_in_a_String.java
@@ -4,29 +4,36 @@ class Solution {
         if (s == null || s.length() == 0)
             return res;
         HashMap<Character, Integer> map = new HashMap<>();
+        // Step 1: Populate the HashMap 'map' with character frequencies from string 'p'
         for (Character c : p.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
         int left = 0;
         int right = 0;
-        int matched = 0;
+        int matched = 0; // Counter to keep track of matching characters
 
+        // Step 2: Begin iterating through the characters of string 's'
         while (right < s.length()) {
             char r = s.charAt(right);
-            right++;
+            right++; // Increment right to extend the window to the right
+
+            // Check if the character 'r' exists in the HashMap 'map'
             if (map.containsKey(r)) {
-                map.put(r, map.get(r) - 1);
+                map.put(r, map.get(r) - 1); // Decrement the character count in 'map'
                 if (map.get(r) == 0)
                     matched++;
             }
 
+            // Step 3: Inner loop to adjust window size and search for anagrams
             while (right - left >= p.length()) {
                 if (matched == map.size()) {
                     res.add(left);
                 }
                 char l = s.charAt(left);
                 left++;
+
+                // Check if the character 'l' exists in the HashMap 'map'
                 if (map.containsKey(l)) {
                     if (map.get(l) == 0)
                         matched--;

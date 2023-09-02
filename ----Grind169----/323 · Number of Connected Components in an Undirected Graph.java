425861a532971a289765d22c229407869082ef94
zhuoww <zhuoww1008@gmail.com>
2023-09-02 11:55:45 -0700

    v1

---
+++ ----Grind169----/323 · Number of Connected Components in an Undirected Graph.java
@@ -0,0 +1,73 @@
+//dfs
+public class Solution {
+    /**
+     * @param n:     the number of vertices
+     * @param edges: the edges of undirected graph
+     * @return: the number of connected components
+     */
+    public int countComponents(int n, int[][] edges) {
+        // write your code here
+        boolean[] visited = new boolean[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(edges, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void dfs(int[][] edges, boolean[] visited, int cur) {
+        if (visited[cur])
+            return;
+        visited[cur] = true;
+
+        for (int[] edge : edges) {
+            if (edge[0] == cur && !visited[edge[1]]) {
+                dfs(edges, visited, edge[1]);
+            }
+
+            if (edge[1] == cur && !visited[edge[0]]) {
+                dfs(edges, visited, edge[0]);
+            }
+        }
+    }
+}
+
+// bfs
+public class Solution {
+    /**
+     * @param n:     the number of vertices
+     * @param edges: the edges of undirected graph
+     * @return: the number of connected components
+     */
+    public int countComponents(int n, int[][] edges) {
+        // write your code here
+        boolean[] visited = new boolean[n];
+        Queue<Integer> que = new LinkedList<>();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                que.offer(i);
+
+                while (!que.isEmpty()) {
+                    int cur = que.poll();
+                    visited[cur] = true;
+                    for (int[] edge : edges) {
+                        if (edge[0] == cur && !visited[edge[1]]) {
+                            que.offer(edge[1]);
+                        }
+                        if (edge[1] == cur && !visited[edge[0]]) {
+                            que.offer(edge[0]);
+                        }
+                    }
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+}

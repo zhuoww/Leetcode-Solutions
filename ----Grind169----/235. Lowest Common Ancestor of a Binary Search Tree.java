aa9cb7cf024f47f4f6fd222b1dd9c7475c720768
zhuoww <zhuoww1008@gmail.com>
2023-09-05 18:49:43 -0700

    v1

--- ----Grind169----/235. Lowest Common Ancestor of a Binary Search Tree.java
+++ ----Grind169----/235. Lowest Common Ancestor of a Binary Search Tree.java
@@ -19,3 +19,21 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+            return null;
+        return dfs(root, p, q);
+    }
+
+    public TreeNode dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return dfs(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return dfs(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}

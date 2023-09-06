fbbe34acdb29ebbaa2d13d4da05debe437d26b5f
zhuoww <zhuoww1008@gmail.com>
2023-09-05 19:46:34 -0700

    v1

---
+++ ----Grind169----/230. Kth Smallest Element in a BST.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    private int res = 0, index = 0;
+
+    public int kthSmallest(TreeNode root, int k) {
+        dfs(root, k);
+        return res;
+    }
+
+    public void dfs(TreeNode root, int k) {
+        if (root == null)
+            return;
+
+        dfs(root.left, k);
+        index++;
+        if (index == k) {
+            res = root.val;
+            return;
+        }
+        dfs(root.right, k);
+
+    }
+}

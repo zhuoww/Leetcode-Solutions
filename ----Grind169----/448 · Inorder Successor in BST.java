0a0944091517a53c13e50b5105019fa27fc39ed9
zhuoww <zhuoww1008@gmail.com>
2023-09-08 21:37:20 -0700

    v1

--- ----Grind169----/448 · Inorder Successor in BST.java
+++ ----Grind169----/448 · Inorder Successor in BST.java
@@ -30,3 +30,25 @@ public class Solution {
         return res;
     }
 }
+
+// dfs
+public class Solution {
+    /*
+     * @param root: The root of the BST.
+     * 
+     * @param p: You need find the successor node of p.
+     * 
+     * @return: Successor of p.
+     */
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        // write your code here
+        if (root == null)
+            return null;
+        if (p.val < root.val) {
+            TreeNode temp = inorderSuccessor(root.left, p);
+            return temp == null ? root : temp;
+        } else {
+            return inorderSuccessor(root.right, p);
+        }
+    }
+}

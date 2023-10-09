363d5efa15b1e2c1757ffeb63d39e6f291ff5012
zhuoww <zhuoww1008@gmail.com>
2023-10-09 15:49:06 -0700

    v1

---
+++ ----Grind169----/383. Ransom Note.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] library = new int[26];
+
+        if (ransomNote.length() > magazine.length())
+            return false;
+
+        for (int i = 0; i < magazine.length(); i++) {
+            library[magazine.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            library[ransomNote.charAt(i) - 'a']--;
+        }
+
+        for (int c : library) {
+            if (c < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

45eb243b5df415795ed30e0b751fe5e45f55a615
zhuoww <zhuoww1008@gmail.com>
2023-10-23 15:48:55 -0700

    v1

---
+++ ----Grind169----/24. Swap Nodes in Pairs.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode cur = dummy;
+
+        while (cur.next != null && cur.next.next != null) {
+            ListNode temp1 = cur.next;
+            ListNode temp2 = cur.next.next.next;
+
+            cur.next = cur.next.next;
+            cur.next.next = temp1;
+            temp1.next = temp2;
+
+            cur = cur.next.next;
+        }
+
+        return dummy.next;
+    }
+}

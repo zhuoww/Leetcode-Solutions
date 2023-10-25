712fb6c05ca352053ac3e6ebf2af7247ea37c4f0
zhuoww <zhuoww1008@gmail.com>
2023-10-24 21:10:20 -0700

    v1

---
+++ ----Grind169----/2. Add Two Numbers.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int v1 = l1 != null ? l1.val : 0;
+            int v2 = l2 != null ? l2.val : 0;
+
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            int remainder = sum % 10;
+
+            ListNode newNode = new ListNode(remainder);
+            cur.next = newNode;
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+            cur = cur.next;
+
+        }
+        if (carry > 0) {
+            ListNode newNode = new ListNode(carry);
+            cur.next = newNode;
+            newNode.next = null;
+        }
+
+        return dummy.next;
+    }
+}

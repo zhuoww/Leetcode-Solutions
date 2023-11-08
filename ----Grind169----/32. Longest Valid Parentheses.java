7248f4ed0eb119ab1613bc7a218926f95bec8ce3
zhuoww <zhuoww1008@gmail.com>
2023-11-08 11:36:03 -0800

    v1

--- ----Grind169----/32. Longest Valid Parentheses.java
+++ ----Grind169----/32. Longest Valid Parentheses.java
@@ -43,3 +43,30 @@ class Solution {
         return max;
     }
 }
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        // stack
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (st.isEmpty()) {
+                    st.push(i);
+                } else {
+                    int len = i - st.peek();
+                    max = Math.max(max, len);
+                }
+            }
+        }
+        return max;
+
+    }
+}

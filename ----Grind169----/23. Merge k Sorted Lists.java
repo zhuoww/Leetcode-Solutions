efe8fa42f80838b78883d11557a2d2f1fafe0846
zhuoww <zhuoww1008@gmail.com>
2023-11-11 19:48:15 -0800

    v1

--- ----Grind169----/23. Merge k Sorted Lists.java
+++ ----Grind169----/23. Merge k Sorted Lists.java
@@ -38,3 +38,40 @@ class Solution {
 
     }
 }
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0)
+            return null;
+        int len = lists.length;
+        int interval = 1;
+
+        while (len > interval) {
+            for (int i = 0; i < len - interval; i += 2 * interval) {
+                lists[i] = merge(lists[i], lists[i + interval]);
+            }
+            interval *= 2;
+        }
+
+        return len > 0 ? lists[0] : null;
+    }
+
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+
+        return dummy.next;
+    }
+}

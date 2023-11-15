0b98871aec41d8f960c8cc32dcd061c91a1a22a1
zhuoww <zhuoww1008@gmail.com>
2023-11-14 21:48:49 -0800

    v1

--- ----Grind169----/621. Task Scheduler.java
+++ ----Grind169----/621. Task Scheduler.java
@@ -1,35 +1,57 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        // maxHeap
+        // maxheap
+        // time: O(nlogn)--adding each element to the max heap takes O(log n) time and
+        // there are n elements
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
         Queue<Pair<Integer, Integer>> que = new LinkedList<>();
-        int[] map = new int[26];
-
+        int[] charMap = new int[26];
         for (char c : tasks) {
-            map[c - 'A']++;
+            charMap[c - 'A']++;
         }
-
-        for (int freq : map) {
-            if (freq > 0) {
-                maxHeap.add(freq);
+        for (int frequent : charMap) {
+            if (frequent > 0) {
+                maxHeap.add(frequent);
             }
         }
 
-        int count = 0;
+        int time = 0;
         while (!maxHeap.isEmpty() || !que.isEmpty()) {
-            count++;
+            time++;
             if (!maxHeap.isEmpty()) {
-                int cur = maxHeap.poll();
-                cur--;
-                if (cur > 0) {
-                    que.add(new Pair(cur, count + n));
+                int val = maxHeap.poll();
+                val--;
+                if (val > 0) {
+                    que.add(new Pair(val, time + n));
                 }
             }
 
-            if (!que.isEmpty() && que.peek().getValue() == count) {
+            if (!que.isEmpty() && que.peek().getValue() == time) {
                 maxHeap.add(que.poll().getKey());
             }
         }
-        return count;
+        return time;
+
+    }
+}
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] charMap = new int[26];// store the frequency of each task
+        for (char c : tasks) {
+            charMap[c - 'A']++;
+        }
+
+        Arrays.sort(charMap);// O(nlogn)
+        int maxFrequent = charMap[25] - 1;
+        int idleSlots = maxFrequent * n;
+
+        for (int i = 24; i >= 0 && charMap[i] > 0; i--) {
+            idleSlots -= Math.min(charMap[i], maxFrequent);
+        }
+
+        idleSlots = Math.max(0, idleSlots); // make sure idleSlots is non-negative
+
+        return idleSlots + tasks.length;
     }
 }

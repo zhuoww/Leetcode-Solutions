678f61e88dd1fc5aa4de9e64cfc47c41ae703bc1
zhuoww <zhuoww1008@gmail.com>
2023-11-18 01:02:43 -0800

    v1

--- ----Grind169----/139. Word Break.java
+++ ----Grind169----/139. Word Break.java
@@ -1,3 +1,4 @@
+//Trie
 class TrieNode {
     char val;
     TrieNode[] children;
@@ -50,5 +51,23 @@ class Solution {
         }
         memo[cur] = 1;
         return false;
+    }
+}
+
+// dp
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[s.length()];
     }
 }

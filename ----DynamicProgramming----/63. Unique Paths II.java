6257a8cbcb48a18c8f23292c2aeadcb9f672b733
zhuoww <zhuoww1008@gmail.com>
2023-12-01 16:14:16 -0800

    v1

---
+++ ----DynamicProgramming----/63. Unique Paths II.java
@@ -0,0 +1,32 @@
+//medium
+//time:O(m*n);space:O(m*n)
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+
+        // 起点或终点遇到障碍说明走不到
+        if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
+            dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}

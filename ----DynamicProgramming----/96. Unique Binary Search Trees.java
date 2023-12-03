55ba3f92e261e37274187170217d686829f74b2d
zhuoww <zhuoww1008@gmail.com>
2023-12-03 11:14:32 -0800

    v1

---
+++ ----DynamicProgramming----/96. Unique Binary Search Trees.java
@@ -0,0 +1,16 @@
+//medium
+//time:O(n^2);space:O(n)
+
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+}

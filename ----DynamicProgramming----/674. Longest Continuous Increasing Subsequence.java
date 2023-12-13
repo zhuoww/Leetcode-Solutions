0b02fffd9da548c21abb266018939d4a0fdc6236
zhuoww <zhuoww1008@gmail.com>
2023-12-13 13:35:01 -0800

    v1

--- ----DynamicProgramming----/674. Longest Continuous Increasing Subsequence.java
+++ ----DynamicProgramming----/674. Longest Continuous Increasing Subsequence.java
@@ -21,3 +21,23 @@ class Solution {
         return res;
     }
 }
+
+// greedy--time:O(n);space:O(1)
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        if (nums.length <= 1)
+            return nums.length;
+        int count = 1;
+        int res = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            res = Math.max(res, count);
+        }
+        return res;
+    }
+}

4580bbab1c9e44d1e21bb0e91ed1ecb49ee219ad
zhuoww <zhuoww1008@gmail.com>
2023-12-18 23:38:55 -0800

    v1

---
+++ ----DynamicProgramming----/647. Palindromic Substrings.java
@@ -0,0 +1,27 @@
+/*
+ * medium
+ * time:O(n^2);space:O(n^2)
+ */
+
+class Solution {
+    public int countSubstrings(String s) {
+        int len = s.length();
+        int res = 0;
+
+        boolean[][] dp = new boolean[len][len];
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = i; j < len; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i <= 1) {
+                        dp[i][j] = true;
+                        res++;
+                    } else if (dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}

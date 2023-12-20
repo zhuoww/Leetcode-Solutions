013877a1545ec2067c5cffb43b9cb63302d435ff
zhuoww <zhuoww1008@gmail.com>
2023-12-20 12:15:16 -0800

    v1

---
+++ ----Grind169----/136. Single Number.java
@@ -0,0 +1,14 @@
+/*
+ * easy
+ * time:O(n);space:O(1)
+ */
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+}

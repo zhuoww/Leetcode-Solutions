ea22ca6f642594a32bb9ba5fab5a6422c25a196f
zhuoww <zhuoww1008@gmail.com>
2023-12-20 12:04:36 -0800

    v1

--- ----Grind169----/191. Number of 1 Bits.java
+++ ----Grind169----/191. Number of 1 Bits.java
@@ -1,9 +1,10 @@
 /*
  * easy
- * time: O(k), where k is the number of set bits in the binary representation of the given integer
- * space: O(1)
  */
 
+//Brian Kernighan's Algorithm
+//time: O(k), where k is the number of set bits in the binary representation of the given integer
+//space: O(1)
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
@@ -11,6 +12,22 @@ public class Solution {
         while (n != 0) {
             n = n & (n - 1);// clears the rightmost set bit in n
             count++;
+        }
+        return count;
+    }
+}
+
+// Bit Manipulation
+// time:O(log n), where n is the value of the input number
+// space:O(1)
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        while (n != 0) {
+            count += n & 1;
+            n >>>= 1;
         }
         return count;
     }

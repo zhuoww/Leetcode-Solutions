922512c21eb213e246ec8eb128a782c98bac91e1
zhuoww <zhuoww1008@gmail.com>
2023-12-23 14:41:58 -0800

    v1

---
+++ ----TopInterview150----/27. Remove Element.java
@@ -0,0 +1,17 @@
+/*
+ * easy
+ * time:O(n);space:O(1)
+ */
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}

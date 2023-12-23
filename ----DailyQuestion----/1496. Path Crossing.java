b94bc40fab6545ee644c7526f67a34efc3918325
zhuoww <zhuoww1008@gmail.com>
2023-12-23 14:28:52 -0800

    v1

--- ----DailyQuestion----/1496. Path Crossing.java
+++ ----DailyQuestion----/1496. Path Crossing.java
@@ -27,3 +27,32 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean isPathCrossing(String path) {
+        HashMap<Character, Pair<Integer, Integer>> map = new HashMap<>();
+        map.put('N', new Pair(0, 1));
+        map.put('S', new Pair(0, -1));
+        map.put('E', new Pair(1, 0));
+        map.put('W', new Pair(-1, 0));
+
+        HashSet<Pair<Integer, Integer>> set = new HashSet<>();
+        set.add(new Pair(0, 0));
+        int x = 0;
+        int y = 0;
+
+        for (Character c : path.toCharArray()) {
+            Pair<Integer, Integer> cur = map.get(c);
+            int dx = cur.getKey();
+            int dy = cur.getValue();
+            x += dx;
+            y += dy;
+
+            Pair p = new Pair(x, y);
+            if (set.contains(p))
+                return true;
+            set.add(p);
+        }
+        return false;
+    }
+}

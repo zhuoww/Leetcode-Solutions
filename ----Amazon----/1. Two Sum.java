a57e5785b46fc00e27637b25a4011a5693768cbb
zhuoww <zhuoww1008@gmail.com>
2023-12-27 13:14:06 -0800

    v1

---
+++ ----Amazon----/1. Two Sum.java
@@ -0,0 +1,30 @@
+/*
+ * easy
+ * time:O(n); space:O(n)
+ */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // map to store the value and index of the element
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // res array to store the output
+        int[] res = new int[2];
+        // base case
+        if (nums == null || nums.length == 0)
+            return res;
+
+        // loop through the nums array
+        for (int i = 0; i < nums.length; i++) {
+            // a variable to store the different between target and the current element
+            int diff = target - nums[i];
+            // check whether the diff is in the map
+            if (map.containsKey(diff)) {
+                res[0] = map.get(diff);
+                res[1] = i;
+            }
+            // otherwise, add the value and index of the element to the map
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+}

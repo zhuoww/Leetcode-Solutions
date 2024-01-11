f0c701ccb550a78934ba8c2fe23e63be4bb8c251
zhuoww <zhuoww1008@gmail.com>
2024-01-10 22:14:39 -0800

    v1

---
+++ ----Salesforce----/139. Word Break.java
@@ -0,0 +1,24 @@
+/*
+ * medium
+ * time: O(n^2), where n is the length of the input string s
+ * space: O(n), where n is the length of the input string s
+ */
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}

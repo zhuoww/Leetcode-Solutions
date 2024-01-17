d567892b0bb9e5feb43d971b0b4d8e694fc1b813
zhuoww <zhuoww1008@gmail.com>
2024-01-16 21:32:46 -0800

    v1

---
+++ ----DailyQuestion----/1207. Unique Number of Occurrences.java
@@ -0,0 +1,20 @@
+/*
+ * easy
+ * time:O(n);space:O(n)
+ */
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int val : map.values()) {
+            set.add(val);
+        }
+
+        return set.size() == map.size();
+    }
+}
